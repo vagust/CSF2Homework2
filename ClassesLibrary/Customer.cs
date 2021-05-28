b0849cb2ade054d2d3767afa955bb21413ad9e5b
lavagust fox <lazelldfox@gmail.com>
2021-05-28 15:33:34 -0500

    com

---
+++ ClassesLibrary/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+   public class Customer : ContactInfo
+    {
+        public string CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+
+    }
+    public Customer(string customerID, string firstName, string lastName) : ()
+}
