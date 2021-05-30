9e9ed11b125520254d9e2d845e63242f13a3cea8
lavagust fox <lazelldfox@gmail.com>
2021-05-29 19:58:57 -0500

    Fin books branch on to the next one

---
+++ ClassesLibrary/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+   public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public Book(string title, string author, int nuberOfPages)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = nuberOfPages;
+        }
+        public Book()
+        {
+            
+        }
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("Book:\n" +
+                "Title: {0}\n" +
+                "Author: {1}\n" +
+                "NumberOfPages: {2}",
+                Title, Author, NumberOfPages);
+        }
+
+    }//class
+}//namespace
