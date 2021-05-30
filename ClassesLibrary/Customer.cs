4a15284db5292105d02a89d8d286a24a787dfecc
lavagust fox <lazelldfox@gmail.com>
2021-05-29 19:01:18 -0500

    filled out cutomer

--- ClassesLibrary/Customer.cs
+++ ClassesLibrary/Customer.cs
@@ -6,13 +6,36 @@ using System.Threading.Tasks;
 
 namespace ClassesLibrary
 {
-   public class Customer : ContactInfo
+    public class Customer
     {
         public string CustomerID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        
+        public ContactInfo ContactInformation { get; set; }
 
+        public Customer(string customerID, string firstName, string lastName, ContactInfo contactInformation )
+        {
+            CustomerID = customerID;
+            FirstName = firstName;
+            LastName = lastName;
+            ContactInformation = contactInformation;
+
+        }
+        public Customer()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString(); 
+            return string.Format("Customer\n" +
+                "CustomerID: {0}\n" +
+                "FirstName: {1}\n" +
+                "LastName: {2}\n" +
+                "ContactInformation:{3}",
+                CustomerID, FirstName, LastName, ContactInformation);
+        }
     }
-    public Customer(string customerID, string firstName, string lastName) : ()
+
 }
